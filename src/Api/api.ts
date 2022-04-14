20866fe91e7ca9c83252244803937eedc53dda41
Noah <noah.cote3@gmail.com>
2022-04-14 18:35:55 -0400

    using https duh

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://us-bank-lien-parcel-analysis.herokuapp.com/nc/us_bank_lien_parcel_analysis_vuxm/api/v1/",
+    "https://us-bank-lien-parcel-analysis.herokuapp.com/nc/us_bank_lien_parcel_analysis_vuxm/api/v1/",
   timeout: 1000,
   headers: {
     "xc-auth": process.env.REACT_APP_API_KEY as string,
