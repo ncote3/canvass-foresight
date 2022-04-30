4fcbf7d5d8b74fbc698deaaef373399897dcc327
Noah <noah.cote3@gmail.com>
2022-04-30 14:06:11 -0400

    Added a loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
-import React, { useEffect, useState } from "react";
-import API from "./Api/api";
+import React, { useEffect, useMemo, useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
+
 import { getAllData } from "./Api/apiHelpers";
 import ImageCardCollection from "./Components/ImageCardCollection/ImageCardCollection";
 import { Sideyard } from "./Components/types";
+import "./App.css";
+
+interface APIFetchStatus {
+  loading: boolean;
+  empty: boolean;
+}
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const initFetchStatus = {
+    loading: false,
+    empty: false,
+  };
+
+  const [fetchStatus, updateFetchStatus] =
+    useState<APIFetchStatus>(initFetchStatus);
   const [sideyards, setSideyards] = useState<Sideyard[]>([]);
 
   useEffect(() => {
     const loadSideyards = async () => {
-      setLoading(true);
+      updateFetchStatus({ ...fetchStatus, loading: true });
 
       const allSideyards: Sideyard[] = await getAllData();
 
-      setSideyards(allSideyards);
-
-      setLoading(false);
+      if (allSideyards) {
+        setSideyards(allSideyards);
+        updateFetchStatus(initFetchStatus);
+      } else {
+        updateFetchStatus({ loading: false, empty: true });
+      }
     };
 
     loadSideyards();
   }, []);
 
-  console.log(sideyards);
+  const _renderContent = useMemo(() => {
+    if (fetchStatus.loading) {
+      return (
+        <div className={"home"}>
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      );
+    }
 
-  return (
-    <div className="App">
+    if (fetchStatus.empty) {
+      return (
+        <div className={"home"}>
+          Server came back empty, try reloading a few times. Email
+          noah.cote3@gmail.com if it persists
+        </div>
+      );
+    }
+
+    return (
       <ImageCardCollection sideyards={sideyards} setSideyards={setSideyards} />
-    </div>
-  );
+    );
+  }, [sideyards, fetchStatus]);
+
+  return <div className="App">{_renderContent}</div>;
 }
 
 export default App;
